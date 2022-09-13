fa44d23ba668a456cd7d1a5ac8a7845c145784ef
Miriani92 <mirian.tsintsadze1992@gmail.com>
2022-09-13 18:41:54 +0400

    adding styles

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Button.module.css";
+import { CartContext } from "../context/cartcontext/Cart-actions";
 
 class Button extends Component {
+  static contextType = CartContext;
   constructor(props) {
     super(props);
   }
   render() {
     const viewBag = this.props.text === "VIEW BAG";
+    const { taggleCart } = this.context;
     return (
       <Link
         style={this.props.style && this.props.style}
         className={styles.button}
         to={viewBag ? "/cart" : "/"}
+        onClick={taggleCart}
       >
         {this.props.text}
       </Link>
